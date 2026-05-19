c8751465acd6f836a49e76714df8b4843f013476
Faustino Rodriguez <Faustinorodriguez0410@gmail.com>
2026-05-19 20:59:14 -0300

    hago la funcion que filtra el plan mas economico y su precio

--- Punto2.py
+++ Punto2.py
@@ -9,3 +9,17 @@ def costo_B(x):
 
 def costo_C(x):
     return -2 * (x ** 2) + 80 * x + 100
+
+# 8. Creo una función para saber cual es el plan más economico
+def plan_mas_economico(x):
+    costo_a = costo_A(x)
+    costo_b = costo_B(x)
+    costo_c = costo_C(x)
+    
+    # Busco el menor costo usando condicionales 
+    if costo_a <= costo_b and costo_a <= costo_c:
+        return "Plan A", costo_a
+    elif costo_b <= costo_a and costo_b <= costo_c:
+        return "Plan B", costo_b
+    else:
+        return "Plan C", costo_c
