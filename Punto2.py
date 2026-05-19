665866d175d7e8a474030a0ec09d4d121ec2d813
Faustino Rodriguez <Faustinorodriguez0410@gmail.com>
2026-05-19 20:20:31 -0300

    Definí las funciones y los modelos de costo e importe la libreria para graficar las funciones

---
+++ Punto2.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+# 5. Defino las funciones y los modelos de costo
+def costo_A(x):
+    return 40 * x + 200
+
+def costo_B(x):
+    return 70 * x + 50
+
+def costo_C(x):
+    return -2 * (x ** 2) + 80 * x + 100
