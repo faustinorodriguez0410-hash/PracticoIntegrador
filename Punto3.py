d32978c5862f25f37e16b00fd5653a600dbbdb7c
Faustino Rodriguez <Faustinorodriguez0410@gmail.com>
2026-05-27 10:04:53 -0300

    arreglamos problema de commits

--- Punto3.py
+++ Punto3.py
@@ -4,6 +4,55 @@ M = [
     [90, 110, 95]
 ]
 
+print("Promedio por función:")
+
+for i in range(3):
+    suma = 0
+
+    for j in range(3):
+        suma = suma + M[i][j]
+
+    promedio = suma / 3
+
+    print("Funcion", i + 1, ":", promedio)
+
+print("\nPromedio por servidor:")
+
+for j in range(3):
+    suma = 0
+
+    for i in range(3):
+        suma = suma + M[i][j]
+
+    promedio = suma / 3
+
+    print("Servidor", j + 1, ":", promedio)
+
+# Matriz transpuesta
+
+MT = []
+
+for j in range(3):
+
+    fila = []
+
+    for i in range(3):
+
+        fila.append(M[i][j])
+
+    MT.append(fila)
+
+print("Matriz transpuesta:")
+
+for fila in MT:
+    print(fila)
+
+M = [
+    [120, 150, 100],
+    [200, 180, 220],
+    [90, 110, 95]
+]
+
 C = [
     [30, 20, 10],
     [15, 25, 20],
